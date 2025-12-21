13217b78a19c84eb02f346f77dbaa2c31ba2b351
bbangddeok2 <songseungil39@gmail.com>
2025-12-21 13:17:37 +0900

    12.20까지 작업중

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
+  $(window).scroll(function () {
+    var pageY = $(window).scrollTop();
+    var sec_2_top = $(".section_2").offset().top;
+
+    if (pageY >= sec_2_top) {
+      $("header").addClass("scrolled");
+    } else {
+      $("header").removeClass("scrolled");
+    }
+  });
+
   var swiper = new Swiper(".swiper_5", {
     slidesPerView: 3.2,
     spaceBetween: 30,
     loop: true,
+    breakpoints: {
+      0: {
+        spaceBetween: 16,
+        slidesPerView: 1.5,
+      },
+      768: {
+        slidesPerView: 3.2,
+      },
+    },
   });
 });
